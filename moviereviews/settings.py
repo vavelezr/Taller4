c4874451524fc44f1e450958aead62c9d784d732
unknown <vavelezr@eafit.edu.co>
2023-05-11 10:33:12 -0500

    allowed

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '2hl5684eyz&6jd+3-rx=vg8enjb+d=f06p^lt^z%%nel1qds_q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['104.197.195.89']
+ALLOWED_HOSTS = ['34.71.41.220']
 
 
 # Application definition
